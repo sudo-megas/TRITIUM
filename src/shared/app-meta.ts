81d379e6f668c1cc48a4deb7fbb89bfe5d8eae23
sudo-megas <halil@namli.xyz>
2026-08-19 02:58:48 +0300

    v0.1.5

--- src/shared/app-meta.ts
+++ src/shared/app-meta.ts
@@ -5,8 +5,8 @@
 // GG/AA/YYYY (XTRITIUM §8).
 
 export const APP_NAME = 'TRITIUM'
-export const APP_VERSION = '0.1.4'
-export const RELEASE_DATE = '18/08/2026'
+export const APP_VERSION = '0.1.5'
+export const RELEASE_DATE = '19/08/2026'
 export const MAKER = 'sudo-megas'
 export const SOURCE_ADDRESS = 'https://github.com/sudo-megas/TRITIUM'
 export const LICENCE_ID = 'GPL-3.0-or-later'
